80008a0308b2241f3c22627723abeaf417b5d6b2
zain391 <76866717+zain391@users.noreply.github.com>
2022-01-29 14:48:55 +0500

    added some more code
    added some more code

--- hello.ts
+++ hello.ts
@@ -1,8 +1,35 @@
 console.log("Hello World");
 // This is an industrial-grade general-purpose greeter function:
-function greetMeM(person, date) {
+function greetMe(person:string, date:Date) {
     
-    console.log(`Hello ${person}, today is ${date}!`);
+    console.log(`Hello ${person}, today is ${date.getTime()}!`);
    }
-   greetMeM("Brendan","i amdsfdk ");
-   
+   greetMe("Brendan",new Date());
+//    string ,number , array , any 
+// these are the most used types in ts
+// const myName: string="Zain";
+// const myAge: number=20;
+// const hobbies: string[]=["one","two","three"];
+// const anyData: any="dkfsjkasd";
+// const bool: boolean=true;
+// const obj: object={}
+
+
+type personType={
+    name:string,
+    age:number,
+    isgraduated:boolean,
+}
+
+
+const person:personType={
+    name:"zain",
+    age:20,
+    isgraduated:true,
+}
+
+const personTwo:personType={
+    name:"zain",
+    age:20,
+    isgraduated:true,
+}
