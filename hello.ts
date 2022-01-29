6f0e1f9595d5e187dd32bdb2b078cecb88c2d14c
zain391 <76866717+zain391@users.noreply.github.com>
2022-01-29 14:33:51 +0500

    added the first code of typescript
    added the first code of typescript

---
+++ hello.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+// This is an industrial-grade general-purpose greeter function:
+function greetMeM(person, date) {
+    
+    console.log(`Hello ${person}, today is ${date}!`);
+   }
+   greetMeM("Brendan","i amdsfdk ");
+   
